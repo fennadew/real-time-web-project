d779dc3809e538708fbf8b2c6de40dc23ea67051
Fenna <fennadewilde@hotmail.com>
2018-04-26 14:28:05 +0200

    working

--- app.js
+++ app.js
@@ -84,17 +84,17 @@ function callBackStations(err, data) {
 }
 
 // Connection URL
-const url = process.env.MONGODB_URI;
+const url = 'mongodb://localhost:27017';
 
 // Database Name
-const dbName = 'conductor';
+const dbName = 'real-time-notifications';
 
 // Use connect method to connect to the server
 MongoClient.connect(url, function (err, client) {
     assert.equal(null, err);
     console.log("Connected successfully to server");
-    // const db = client.db(dbName);
-    const notificationsCollection = client.collection('notifications');
+    const db = client.db(dbName);
+    const notificationsCollection = db.collection('notifications');
 
     const dataUser = {
         arrivalTime: '',
